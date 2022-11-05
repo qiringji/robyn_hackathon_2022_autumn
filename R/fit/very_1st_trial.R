3f62ad24c7e9df094dae040e8f5327551e81144a
onysuke <hyodo8864@gmail.com>
2022-11-05 18:11:07 +0900

    Add calibration part.

--- R/fit/very_1st_trial.R
+++ R/fit/very_1st_trial.R
@@ -1,9 +1,11 @@
+####### Stage1. Data Generation #######
 # install siMMMulator 
 remotes::install_github(
   repo = "facebookexperimental/siMMMulator"
 )
 
 library(siMMMulator)
+revenue_per_conv <- 1
 
 my_variables <- step_0_define_basic_parameters(
   years = 5,
@@ -11,7 +13,7 @@ my_variables <- step_0_define_basic_parameters(
   channels_clicks = c(),
   frequency_of_campaigns = 1,
   true_cvr = c(0.001, 0.002),
-  revenue_per_conv = 1
+  revenue_per_conv = revenue_per_conv
 )
 
 df_baseline <- step_1_create_baseline(
@@ -152,9 +154,9 @@ df_final %>%
 df_ads_step7
 
 df_final_cv = dplyr::inner_join(df_final, df_ads_step7, by='DATE')
+colnames(df_final_cv)[2] <- 'revenue'
 
-df_final_cv.column
-
+####### Stage2. MMM #######
 # Robyn
 library(Robyn) # remotes::install_github("facebookexperimental/Robyn/R")
 
@@ -168,6 +170,8 @@ options(future.fork.enable = TRUE)
 
 library("reticulate") # Load the library
 
+df_final_cv
+
 # Directory where you want to export results to (will create new folders)
 robyn_object <- "~/Desktop"
 
@@ -175,7 +179,7 @@ InputCollect <- robyn_inputs(
   dt_input = df_final_cv,
   dt_holidays = dt_prophet_holidays,
   date_var = "DATE", # date format must be "2020-01-01"
-  dep_var = "total_revenue.x", # there should be only one dependent variable
+  dep_var = "revenue", # there should be only one dependent variable
   dep_var_type = "revenue", # "revenue" (ROI) or "conversion" (CPA)
   prophet_vars = c("trend", "season", "holiday"), # "trend","season", "weekday" & "holiday"
   prophet_country = "DE", # input one country. dt_prophet_holidays includes 59 countries by default
@@ -226,16 +230,50 @@ if (length(InputCollect$exposure_vars) > 0) {
   InputCollect$modNLS$plots$search_clicks_P
 }
 
+######################
+# Calibration
+
+# calculation of lift, sum(conversion) * revenue_per_cv.
+df_range <- df_final_cv %>% filter(DATE >= "2017-01-01" & DATE <= "2017-03-05")
+df_range
+calib_lift <- sum(df_range[c("conv_Facebook")]) * revenue_per_conv
+calib_spend <- sum(df_range[c("spend_Facebook.x")])
+
+calibration_input <- data.frame(
+  # channel name must in paid_media_vars
+  channel = c("spend_Facebook.x"),
+  # liftStartDate must be within input data range
+  liftStartDate = as.Date(c("2017-01-01")),
+  # liftEndDate must be within input data range
+  liftEndDate = as.Date(c("2017-03-05")),
+  # Provided value must be tested on same campaign level in model and same metric as dep_var_type
+  liftAbs = c(calib_lift),
+  # Spend within experiment: should match within a 10% error your spend on date range for each channel from dt_input
+  spend = c(calib_spend),
+  # Confidence: if frequentist experiment, you may use 1 - pvalue
+  confidence = c(0.95),
+  # KPI measured: must match your dep_var
+  metric = c("revenue"),
+  # Either "immediate" or "total". For experimental inputs like Facebook Lift, "immediate" is recommended.
+  calibration_scope = c("immediate")
+)
+InputCollect <- robyn_inputs(InputCollect = InputCollect, calibration_input = calibration_input)
+
+
 ################################################################
 #### Step 3: Build initial model
-
+# Try to use nevergrads.
+# conda_create("r-reticulate", "python=3.9")
+# use_condaenv("r-reticulate")
+# conda_install("r-reticulate", "nevergrad", pip=TRUE)
+# Sys.setenv(RETICULATE_PYTHON = "/Users/s11616/opt/anaconda3/envs/r-reticulate/bin/python")
 ## Run all trials and iterations. Use ?robyn_run to check parameter definition
 OutputModels <- robyn_run(
   InputCollect = InputCollect, # feed in all model specification
   # cores = NULL, # default to max available
   # add_penalty_factor = FALSE, # Untested feature. Use with caution.
-  iterations = 500, # recommended for the dummy dataset
-  trials = 2, # recommended for the dummy dataset
+  iterations = 2000, # recommended for the dummy dataset
+  trials = 5, # recommended for the dummy dataset
   outputs = FALSE # outputs = FALSE disables direct model output - robyn_outputs()
 )
 print(OutputModels)
@@ -262,185 +300,11 @@ print(OutputCollect)
 
 ## Compare all model one-pagers and select one that mostly reflects your business reality
 print(OutputCollect)
-select_model <- "1_11_12" # Pick one of the models from OutputCollect to proceed
+select_model <- "1_105_3" # Pick one of the models from OutputCollect to proceed
 
 #### Since 3.7.1: JSON export and import (faster and lighter than RDS files)
 ExportedModel <- robyn_write(InputCollect, OutputCollect, select_model)
 print(ExportedModel)
-
-################################################################
-#### Step 5: Get budget allocation based on the selected model above
-
-## Budget allocation result requires further validation. Please use this recommendation with caution.
-## Don't interpret budget allocation result if selected model above doesn't meet business expectation.
-
-# Check media summary for selected model
-print(ExportedModel)
-
-# Run ?robyn_allocator to check parameter definition
-# Run the "max_historical_response" scenario: "What's the revenue lift potential with the
-# same historical spend level and what is the spend mix?"
-AllocatorCollect1 <- robyn_allocator(
-  InputCollect = InputCollect,
-  OutputCollect = OutputCollect,
-  select_model = select_model,
-  scenario = "max_historical_response",
-  channel_constr_low = 0.7,
-  channel_constr_up = c(1.2, 1.5),
-  export = TRUE,
-  date_min = "2017-01-01",
-  date_max = "2021-12-19"
-)
-print(AllocatorCollect1)
-# plot(AllocatorCollect1)
-
-# Run the "max_response_expected_spend" scenario: "What's the maximum response for a given
-# total spend based on historical saturation and what is the spend mix?" "optmSpendShareUnit"
-# is the optimum spend share.
-AllocatorCollect2 <- robyn_allocator(
-  InputCollect = InputCollect,
-  OutputCollect = OutputCollect,
-  select_model = select_model,
-  scenario = "max_response_expected_spend",
-  channel_constr_low = c(0.7, 0.7),
-  channel_constr_up = c(1.2, 1.5),
-  expected_spend = 1000000, # Total spend to be simulated
-  expected_spend_days = 7, # Duration of expected_spend in days
-  export = TRUE
-)
-print(AllocatorCollect2)
-AllocatorCollect2$dt_optimOut
-# plot(AllocatorCollect2)
-
-## A csv is exported into the folder for further usage. Check schema here:
-## https://github.com/facebookexperimental/Robyn/blob/main/demo/schema.R
-
-## QA optimal response
-# Pick any media variable: InputCollect$all_media
-select_media <- "spend_Facebook.x"
-# For paid_media_spends set metric_value as your optimal spend
-metric_value <- AllocatorCollect1$dt_optimOut$optmSpendUnit[
-  AllocatorCollect1$dt_optimOut$channels == select_media
-]; metric_value
-# # For paid_media_vars and organic_vars, manually pick a value
-# metric_value <- 10000
-
-if (TRUE) {
-  optimal_response_allocator <- AllocatorCollect1$dt_optimOut$optmResponseUnit[
-    AllocatorCollect1$dt_optimOut$channels == select_media
-  ]
-  optimal_response <- robyn_response(
-    InputCollect = InputCollect,
-    OutputCollect = OutputCollect,
-    select_model = select_model,
-    select_build = 0,
-    media_metric = select_media,
-    metric_value = metric_value
-  )
-  plot(optimal_response$plot)
-  if (length(optimal_response_allocator) > 0) {
-    cat("QA if results from robyn_allocator and robyn_response agree: ")
-    cat(round(optimal_response_allocator) == round(optimal_response$response), "( ")
-    cat(optimal_response$response, "==", optimal_response_allocator, ")\n")
-  }
-}
-
-################################################################
-#### Step 6: Model refresh based on selected model and saved results "Alpha" [v3.7.1]
-
-## Must run robyn_write() (manually or automatically) to export any model first, before refreshing.
-## The robyn_refresh() function is suitable for updating within "reasonable periods".
-## Two situations are considered better to rebuild model:
-## 1. most data is new. If initial model has 100 weeks and 80 weeks new data is added in refresh,
-## it might be better to rebuild the model. Rule of thumb: 50% of data or less can be new.
-## 2. new variables are added.
-
-# Provide JSON file with your InputCollect and ExportedModel specifications
-# It can be any model, initial or a refresh model
-json_file <- "~/Desktop/Robyn_202211042030_init/RobynModel-1_11_12.json"
-RobynRefresh <- robyn_refresh(
-  json_file = json_file,
-  dt_input = df_final_cv,
-  dt_holidays = dt_prophet_holidays,
-  refresh_steps = 13,
-  refresh_iters = 1000, # 1k is an estimation
-  refresh_trials = 1
-)
-
-
-################################################################
-#### Step 7: Get budget allocation recommendation based on selected refresh runs
-
-# Run ?robyn_allocator to check parameter definition
-AllocatorCollect <- robyn_allocator(
-  InputCollect = InputCollect,
-  OutputCollect = OutputCollect,
-  select_model = select_model,
-  scenario = "max_response_expected_spend",
-  channel_constr_low = c(0.7, 0.7),
-  channel_constr_up = c(1.2, 1.5),
-  expected_spend = 2000000, # Total spend to be simulated
-  expected_spend_days = 14 # Duration of expected_spend in days
-)
-print(AllocatorCollect)
-# plot(AllocatorCollect)
-
-################################################################
-#### Step 8: get marginal returns
-
-
-# Get response for 80k from result saved in robyn_object
-Spend1 <- 60000
-Response1 <- robyn_response(
-  InputCollect = InputCollect,
-  OutputCollect = OutputCollect,
-  select_model = select_model,
-  media_metric = "impressions_Facebook.x",
-  metric_value = Spend1
-)
-Response1$response / Spend1 # ROI for search 80k
-Response1$plot
-
-
-# Get response for +10%
-Spend2 <- Spend1 * 1.1
-Response2 <- robyn_response(
-  InputCollect = InputCollect,
-  OutputCollect = OutputCollect,
-  select_model = select_model,
-  media_metric = "impressions_Facebook.x",
-  metric_value = Spend2
-)
-Response2$response / Spend2 # ROI for search 81k
-Response2$plot
-
-# Marginal ROI of next 1000$ from 80k spend level for search
-(Response2$response - Response1$response) / (Spend2 - Spend1)
-
-## Example of getting paid media exposure response curves
-imps <- 50000000
-response_imps <- robyn_response(
-  InputCollect = InputCollect,
-  OutputCollect = OutputCollect,
-  select_model = select_model,
-  media_metric = "impressions_Facebook.x",
-  metric_value = imps
-)
-response_imps$response / imps * 1000
-response_imps$plot
-
-## Example of getting organic media exposure response curves
-sendings <- 30000
-response_sending <- robyn_response(
-  InputCollect = InputCollect,
-  OutputCollect = OutputCollect,
-  select_model = select_model,
-  media_metric = "impressions_Facebook.x",
-  metric_value = sendings
-)
-response_sending$response / sendings * 1000
-response_sending$plot
-
 
 ############ WRITE ############
 # Manually create JSON file with inputs data only
@@ -448,31 +312,3 @@ robyn_write(InputCollect, dir = "~/Desktop")
 
 # Manually create JSON file with inputs and specific model results
 robyn_write(InputCollect, OutputCollect, select_model)
-
-
-# TODO: calculate lift (sum * revenue_per_cv)
-df_final_cv %>% filter(DATE >= "2017-01-01" & DATE <= "2017-03-05")
-
-######################
-# Calibration
-calibration_input <- data.frame(
-   # channel name must in paid_media_vars
-   channel = c("spend_Facebook.x"),
-   # liftStartDate must be within input data range
-   liftStartDate = as.Date(c("2018-05-01")),
-   # liftEndDate must be within input data range
-   liftEndDate = as.Date(c("2017-03-05")),
-   # Provided value must be tested on same campaign level in model and same metric as dep_var_type
-   liftAbs = c(400000),
-   # Spend within experiment: should match within a 10% error your spend on date range for each channel from dt_input
-   spend = c(421000),
-   # Confidence: if frequentist experiment, you may use 1 - pvalue
-   confidence = c(0.95),
-   # KPI measured: must match your dep_var
-   metric = c("revenue"),
-   # Either "immediate" or "total". For experimental inputs like Facebook Lift, "immediate" is recommended.
-   calibration_scope = c("immediate")
- )
-InputCollect <- robyn_inputs(InputCollect = InputCollect, calibration_input = calibration_input)
-
-# TODO：refresh
